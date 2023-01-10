8e057ac50deb71fc4063f64f995454fc85bf3b26
vagrant <girish21180@gmail.com>
2023-01-10 01:41:57 +0000

    My first commit

---
+++ ansible.sh
@@ -0,0 +1,7 @@
+sudo yum update -y
+sudo yum install epel-release
+sudo yum install epel-release
+install python and ansible
+sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel epel-release
+sudo yum install -y python-pip python-wheel
+sudo pip install ansible -y
